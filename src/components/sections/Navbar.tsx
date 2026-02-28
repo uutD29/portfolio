44d538caa72dd9435f2e6f7bd39df533e52ffa98
uhud <uhud76@gmail.com>
2026-03-01 02:50:18 +0700

    first commit

--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -1,20 +1,22 @@
 import { ModeToggle } from "@/components/mode-toggle"
-import logoBlack from "@/assets/logo-b.png"
-import logoWhite from "@/assets/logo-w.png"
 
 const Navbar = () => {
   return (
     <header className="sticky top-0 z-50 border-b bg-background">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
         
+        {/* Logo */}
         <div>
+          {/* Light Mode Logo */}
           <img
-            src={logoBlack}
+            src="/logo-b.png"
             alt="Logo"
             className="h-4 dark:hidden"
           />
+
+          {/* Dark Mode Logo */}
           <img
-            src={logoWhite}
+            src="/logo-w.png"
             alt="Logo"
             className="hidden h-4 dark:block"
           />
