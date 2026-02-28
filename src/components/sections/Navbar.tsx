1d78dc33cf8b4b3d34ed823fb249dd94d8cb7fdf
uhud <uhud76@gmail.com>
2026-03-01 02:21:49 +0700

    final fix github pages assets

--- src/components/sections/Navbar.tsx
+++ src/components/sections/Navbar.tsx
@@ -1,22 +1,19 @@
 import { ModeToggle } from "@/components/mode-toggle"
 
 const Navbar = () => {
+  const base = import.meta.env.BASE_URL
+
   return (
     <header className="sticky top-0 z-50 border-b bg-background">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
-        
-        {/* Logo */}
         <div>
-          {/* Light Mode Logo */}
           <img
-            src="/logo-b.png"
+            src={`${base}logo-b.png`}
             alt="Logo"
             className="h-4 dark:hidden"
           />
-
-          {/* Dark Mode Logo */}
           <img
-            src="/logo-w.png"
+            src={`${base}logo-w.png`}
             alt="Logo"
             className="hidden h-4 dark:block"
           />
