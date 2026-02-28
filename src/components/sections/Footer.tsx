7da98c1f3964cdeec85e1ff4b3712636099133de
uutD29 <uhud76@gmail.com>
2026-03-01 02:56:20 +0700

    Update Footer.tsx

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="border-t mt-24">
+    <footer className="border-t mt-24 pt-8">
       <div className="mx-auto text-center text-sm text-muted-foreground">
         © {new Date().getFullYear()} Uhud Tarbani. All rights reserved.
       </div>
